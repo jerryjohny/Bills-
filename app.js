627433f9a4c684a1af47387bc53157fb9f91d7bb
Germano Kazembe <johnyjerry53@gmail.com>
2022-06-11 22:29:11 +0200

    Todasa as rotas verificadas

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const morgan = require('morgan');
 const express = require('express');
 const app= express();
 const rota_usuarios= require('./api/routs/Users');
-
 const rota_contactos= require('./api/routs/contacto');
+const rota_localizacao= require('./api/routs/localizacao');
+const rota_declaracao= require('./api/routs/declaracao');
+const rota_pagamento= require('./api/routs/pagamento');
 const rota_empresa= require('./api/routs/empresa');
+const rota_iva= require('./api/routs/iva');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
@@ -38,6 +41,10 @@ app.use((req,res,next)=>{
 app.use('/user',rota_usuarios);
 app.use('/contacto',rota_contactos);
 app.use('/empresa',rota_empresa);
+app.use('/iva',rota_iva);
+app.use('/localizacao',rota_localizacao)
+app.use('/declaracao',rota_declaracao)
+app.use('/pagamento',rota_pagamento)
 
 //rotas fall-back
 
